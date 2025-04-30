7cd5bf8e7961874c20da2aeb09644663a4e173c8
Nishu0 <itsthakkarnisarg@gmail.com>
2025-04-30 17:36:10 +0530

    :zap: Header and Navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,64 @@
-import Image from "next/image";
+import { AnimatedGridPattern } from "@/components/grid";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      Hello
-    </>
+    <main className="flex flex-1 flex-col items-center justify-center relative overflow-hidden bg-black min-h-screen">
+      {/* Multiple layered backgrounds for depth */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808015_1px,transparent_1px),linear-gradient(to_bottom,#80808015_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808008_1px,transparent_1px),linear-gradient(to_bottom,#80808008_1px,transparent_1px)] bg-[size:64px_64px]"></div>
+      
+      {/* Animated grid pattern */}
+      <AnimatedGridPattern
+        numSquares={40}
+        maxOpacity={0.15}
+        duration={4}
+        repeatDelay={0.5}
+        className={cn(
+          "absolute inset-0 z-0",
+          "[mask-image:radial-gradient(900px_circle_at_center,white,transparent)]"
+        )}
+      />
+      
+      {/* Gradient overlays */}
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-black/60 to-transparent z-0 opacity-70"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-transparent to-black/80 z-0"></div>
+      
+      {/* Hero content */}
+      <div className="container relative z-10 mx-auto flex flex-col items-center justify-center space-y-8 py-6 px-4 text-center max-w-4xl">
+        <Badge className="rounded-full border-zinc-800 bg-zinc-900/70 backdrop-blur-sm px-4 py-1 text-white">
+          Onchain Hackathon Selection Platform
+        </Badge>
+        
+        <div className="space-y-4">
+          <h1 className="font-serif text-6xl font-bold italic tracking-tight md:text-7xl lg:text-8xl text-white">FBI</h1>
+          <p className="text-xl text-zinc-400 md:text-2xl">/ Find Builders Intelligently /</p>
+        </div>
+        
+        <p className="max-w-[600px] text-zinc-400 text-lg">
+          The ultimate platform for hackathon organizers to discover and evaluate top blockchain talent based on
+          verifiable onchain credentials and contributions.
+        </p>
+        
+        <div className="flex flex-wrap gap-4 justify-center pt-4">
+          <Link href="/get-started">
+            <Button className="rounded-full bg-white text-black hover:bg-zinc-200 px-8 py-6 text-lg font-medium">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="/learn-more">
+            <Button
+              variant="outline"
+              className="rounded-full border-zinc-800 bg-zinc-950/50 backdrop-blur-sm text-white hover:bg-zinc-900 hover:text-white px-8 py-6 text-lg font-medium"
+            >
+              Learn More
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,199 @@
+// components/Navbar.tsx
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useTheme } from "next-themes";
+import { Github } from "lucide-react";
+export default function Navbar() {
+  const { theme, setTheme } = useTheme();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  // Toggle dark mode
+  const toggleDarkMode = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  // Toggle mobile menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="pt-2">
+    <div className="sticky top-0 z-50 border shadow-md dark:shadow-zinc-900/20 shadow-zinc-300/30 bg-background/85 backdrop-blur-md rounded-xl mx-2">
+      <nav data-orientation="horizontal" className="mx-auto flex h-16 flex-row items-center px-6 lg:px-8" dir="ltr">
+        {/* Left side elements */}
+        <Link href="/" className="inline-flex items-center gap-2.5 font-semibold">
+          <p className="font-serif italic text-2xl">FBI</p>
+        </Link>
+        
+        
+        {/* Right side elements */}
+        <div className="flex flex-row items-center justify-end gap-1.5 flex-1">
+          {/* Mobile Search Button */}
+          <button 
+            type="button" 
+            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors duration-100 disabled:pointer-events-none disabled:opacity-50 hover:bg-fd-accent hover:text-fd-accent-foreground p-1.5 [&_svg]:size-5 lg:hidden"
+            data-search=""
+            aria-label="Open Search"
+          >
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="24" 
+              height="24" 
+              viewBox="0 0 24 24" 
+              fill="none" 
+              stroke="currentColor" 
+              strokeWidth="2" 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              className="lucide p-px"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </svg>
+          </button>
+          
+          {/* Desktop Search Bar */}
+        
+          
+          {/* Theme Toggle */}
+          <button 
+            className="inline-flex items-center rounded-full border p-1 max-lg:hidden" 
+            aria-label="Toggle Theme" 
+            data-theme-toggle=""
+            onClick={toggleDarkMode}
+          >
+            {theme === "dark" ? (
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="currentColor" 
+                stroke="currentColor" 
+                strokeWidth="2" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                className="lucide size-6.5 rounded-full p-1.5 bg-fd-accent text-fd-accent-foreground"
+              >
+                <circle cx="12" cy="12" r="4"></circle>
+                <path d="M12 2v2"></path>
+                <path d="M12 20v2"></path>
+                <path d="m4.93 4.93 1.41 1.41"></path>
+                <path d="m17.66 17.66 1.41 1.41"></path>
+                <path d="M2 12h2"></path>
+                <path d="M20 12h2"></path>
+                <path d="m6.34 17.66-1.41 1.41"></path>
+                <path d="m19.07 4.93-1.41 1.41"></path>
+              </svg>
+            ) : (
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="currentColor" 
+                stroke="currentColor" 
+                strokeWidth="2" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                className="lucide size-6.5 rounded-full p-1.5 text-fd-muted-foreground"
+              >
+                <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+              </svg>
+            )}
+          </button>
+        </div>
+        
+        {/* GitHub and Mobile Menu */}
+        <ul className="flex flex-row items-center gap-2">
+          <li className="list-none">
+            <a 
+              href="https://github.com/" 
+              rel="noreferrer noopener" 
+              target="_blank" 
+              className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors duration-100 disabled:pointer-events-none disabled:opacity-50 hover:bg-fd-accent hover:text-fd-accent-foreground p-1.5 [&_svg]:size-5 -me-1.5 max-lg:hidden" 
+              aria-label="GitHub" 
+              data-active="false"
+            >
+              <Github className="h-5 w-5" />
+            </a>
+          </li>
+          <li className="list-none">
+            <a 
+              href="https://github.com/" 
+              rel="noreferrer noopener" 
+              target="_blank" 
+              className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors duration-100 disabled:pointer-events-none disabled:opacity-50 hover:bg-fd-accent hover:text-fd-accent-foreground p-1.5 [&_svg]:size-5 -me-1.5 max-lg:hidden" 
+              aria-label="GitHub" 
+              data-active="false"
+            >
+              <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.99 0H20.298L13.071 8.26004L21.573 19.5H14.916L9.70202 12.683L3.73597 19.5H0.426L8.15602 10.665L0 0H6.82602L11.539 6.23104L16.99 0ZM15.829 17.52H17.662L5.83002 1.876H3.86297L15.829 17.52Z" fill="#ffffff"></path></svg>
+            </a>
+          </li>
+          <li className="list-none lg:hidden">
+            <button 
+              id="mobile-menu-trigger" 
+              data-state={isMenuOpen ? "open" : "closed"} 
+              aria-expanded={isMenuOpen}
+              aria-controls="mobile-menu-content"
+              onClick={toggleMenu}
+              className="data-[state=open]:bg-fd-accent/50 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors duration-100 disabled:pointer-events-none disabled:opacity-50 hover:bg-fd-accent hover:text-fd-accent-foreground p-1.5 [&_svg]:size-5 group -me-2" 
+              aria-label="Toggle Menu"
+            >
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="2" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                className="lucide size-3 transition-transform duration-300 group-data-[state=open]:rotate-180"
+              >
+                <path d="m6 9 6 6 6-6"></path>
+              </svg>
+            </button>
+          </li>
+        </ul>
+        
+      </nav>
+      
+      {/* Mobile Menu Content (shown when isMenuOpen is true) */}
+      {isMenuOpen && (
+        <div 
+          id="mobile-menu-content"
+          className="lg:hidden absolute w-full bg-fd-background border-t border-fd-border py-2 px-4 z-50"
+        >
+          <ul className="space-y-2">
+            <li>
+              <Link 
+                href="https://x.com/" 
+                className="block p-2 text-sm text-fd-muted-foreground hover:text-fd-accent-foreground"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Twitter
+              </Link>
+            </li>
+            <li>
+              <a 
+                href="https://github.com/"
+                rel="noreferrer noopener"
+                target="_blank"
+                className="block p-2 text-sm text-fd-muted-foreground hover:text-fd-accent-foreground"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+    </div>
+  );
+}
