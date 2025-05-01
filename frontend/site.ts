856f10fbe8a6bc6b16fb5d580227af15195089b9
Nishu0 <itsthakkarnisarg@gmail.com>
2025-05-02 00:50:51 +0530

    Siteconfig Added

---
+++ frontend/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "FBI: Find Builders Intelligently",
+    description:
+      "FBI: Find Builders Intelligently - The ultimate platform for hackathon organizers to discover and evaluate top blockchain talent based on verifiable onchain credentials and contributions.",
+    ogImage: "https://res.cloudinary.com/dyk5s8gbw/image/upload/v1746126866/qvjvjpdfsexmewrxqdvo.png",
+    twitter: "https://x.com/",
+    github: "",
+    linkedin: "",
+    author: "Nisarg Thakkar",
+    url: "",
+  } as const;
