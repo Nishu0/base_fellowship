0b6e7a38a66ef3dd77d4a8cd79a085250b8b7a20
mbcse <mbcse50@gmail.com>
2025-05-08 20:06:18 +0530

    Fixed

--- backend/src/common/utils/envConfig.ts
+++ backend/src/common/utils/envConfig.ts
@@ -10,7 +10,7 @@ export const env = cleanEnv(process.env, {
   CORS_ORIGIN: str({ devDefault: [
     "http://localhost:3001",
     "http://localhost:3000",
-    "https://klyro.com"
+    "https://klyro.dev"
   ].join(',') }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
