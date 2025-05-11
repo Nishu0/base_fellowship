d3dfd80e1e2fa1a0571740c6fb124e01844f6dad
Nishu0 <itsthakkarnisarg@gmail.com>
2025-05-11 19:55:09 +0530

    updated env config

--- backend/src/common/utils/envConfig.ts
+++ backend/src/common/utils/envConfig.ts
@@ -11,7 +11,7 @@ export const env = cleanEnv(process.env, {
     "http://localhost:3001",
     "http://localhost:3000",
     "https://klyro.dev",
-    "https://www.klyro.dev/"
+    "https://www.klyro.dev"
   ].join(',') }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
