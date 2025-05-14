6a64e5d3c340b9a2c9433f28183c281b6ff28bb1
Nishu0 <itsthakkarnisarg@gmail.com>
2025-05-14 12:28:19 +0530

    speed insights

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,7 @@ import { siteConfig } from "@/site";
 import ContactModal from "@/components/Contact";
 import { Analytics } from "@vercel/analytics/next"
 import { PostHogProvider } from "@/providers/posthog";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -75,6 +76,7 @@ export default function RootLayout({
           <Navbar />
           <PostHogProvider>
             {children}
+            <SpeedInsights />
           </PostHogProvider>
           <Analytics/>
         </ThemeProvider>
